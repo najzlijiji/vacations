15be133b434ea4a38348ffb351b75072ffe80def
Borko Kucin <najzlijiji@gmail.com>
2017-06-27 20:50:01 +0200

    added connection class

---
+++ src/connection.php
@@ -0,0 +1,16 @@
+<?php
+  class Db {
+    private static $instance = NULL;
+
+    private function __construct() {}
+
+    private function __clone() {}
+
+    public static function getInstance() {
+      if (!isset(self::$instance)) {
+        self::$instance = mysqli('localhost', 'root', 'redstar', 'vacations');
+      }
+      return self::$instance;
+    }
+  }
+?>
